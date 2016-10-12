ce70bceabf737b6028767e336d8c95c2fe00cc8c
Brandi Phillips <milligan.brandi@gmail.com>
2016-10-12 13:28:28 -0700

    top ten method working

--- app/controllers/all_media_controller.rb
+++ app/controllers/all_media_controller.rb
@@ -1,8 +1,9 @@
 class AllMediaController < ApplicationController
   def index
     # three lists, top 10 of each media type (movies, books, albums).  Lists are generated from the contents of the databases or params since the information will be brought in via forms.  Take a look at Task list...
-    @movie = Movie.all
-    @books = Book.all
-    @albums = Album.all
+    @albums = Album.top_ten
+    @books = Book.top_ten
+    @movies = Movie.top_ten
+
   end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,9 @@
 class Album < ActiveRecord::Base
+
+  def self.top_ten
+    albums = Album.all.order(:ranked)
+    top = albums.take(10)
+    return top
+  end
+
 end
